d39468919f6bd55081100254d65f1294ae0194f5
Seeam Islam <seeamislam25@gmail.com>
2019-09-17 11:01:10 -0400

    Add: Yargs (enables the entry of any city)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const request = require('request'); // making an API call using request
+const argv = require('yargs').argv; // integrate yargs. Yargs works by exposing any variables we use in the console onto the argv object.
 
 let apiKey = '927b698f4cabb4200220709d2630004b'; // create a variable for the API key
-let city = 'brampton'; // created a city which we would like to pass
+let city = argv.c || 'Brampton'; // c is the property of argv which denotes the city. If no city is inputted, use Brampton
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 // created a variable and assigned it to the OpenMaps URL with its two required parameters (as shown in the documentation)
 
@@ -10,10 +11,12 @@ request(url, function (err, response, body) { // We pass in a url, and request r
   if(err){
     console.log('error:', error); // We check for an error in our request. If there is one, we log the error and are done.
   } 
-  else { // If there is no error, we log the entire contents of the response body.
+  else { // If there is no error, we log the entire content of the response body.
     let weather = JSON.parse(body) // convert the jumbled text (JSON) into a JS object. Now, we can fetch the information of the object to get what we need. 
 
     let message = `It's ${weather.main.temp} degrees in ${weather.name}!`; // fetch temperature in the city we set
     console.log(message); // print the temp to the console  
   }
 });
+
+// As input, we now do node.js -c cityName
