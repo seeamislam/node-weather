7deb6c5c754bd608a4c9fd149834848dc4146090
Seeam Islam <seeamislam25@gmail.com>
2019-09-17 11:16:51 -0400

    Add: ejs for dynamic HTML

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 const express = require('express') // use the required function to fetch Express
 const app = express() // create an instance named app by invoking express
 
+app.set('view engine', 'ejs') // set up EJS which allows us to interact with variables and then dynamically create our HTML page
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    app.get('/', function (req, res) {
+        res.render('index');
+      })
 })
 
 app.listen(3000, function () { // we are creating a server that is listening to port3000 for connection
